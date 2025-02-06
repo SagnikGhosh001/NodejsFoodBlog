3faee631ce4d5e943a5ed4558d57c6dad3d76cca
SagnikGhosh001 <sagnikghosh904@gmail.com>
2025-02-06 21:35:54 +0530

    Removed config folder from tracking

--- config/swagger.js
+++
@@ -1,7 +0,0 @@
-const {Express,Request,Response} = require('express');
-const swaggerjsdoc = require('swagger-jsdoc');
-const swaggerui = require('swagger-ui-express');
-const {version} = require('../package.json');
-
-
-
